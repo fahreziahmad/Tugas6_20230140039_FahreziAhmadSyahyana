4820136d92d14de7e640075ddff5f01bff19f424
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2025-04-29 18:45:41 +0700

    first commit

---
+++ java.js
@@ -0,0 +1,15 @@
+const video = document.querySelector("video");
+const audio = document.querySelector("audio");
+const gambar = document.querySelector("img");
+
+video.addEventListener("click", function video(){
+    alert("MV Rapsodi JKT48 Cuy");
+});
+
+audio.addEventListener("play", function audio(){
+    alert("Audio Rapsodi Cuy");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("Gambar Album Rapsodi JKT 48 Cuy");
+});
